956f8bbf2dabfde9186867493efbac58523d7aa6
Dustin Kimmel <dkimmel@gmail.com>
2018-06-05 15:45:23 -0700

    create basic server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || process.env.APP_PORT_NUMBER;
+
+// attach middleware
+app.use(bodyParser.json());
+
+// routes
+app.get('/', function (req, res) {
+  res.send('heartbeat');
+});
+
+// start server
+app.listen(port, function () {
+  console.log('Server listening on port ' + port + '...');
+});
