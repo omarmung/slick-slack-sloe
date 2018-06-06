da43b1d036163f0b28463236d77234f8a7179bf5
Dustin Kimmel <dkimmel@gmail.com>
2018-06-05 20:45:27 -0700

    add api doc generator

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
-router
-  .get('/', (req, res) => { res.send('GET handler for /api/v1 route.') })
-  .post('/', function(req, res) { res.send('POST handler for /api/v1 route.') })
+/**
+ * @api {get} /api/v1 App Heartbeat
+ * @apiName Heartbeat
+ * @apiGroup General
+ *
+ * @apiSuccess {String} status Service status indicator
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "status": "ok"
+ *     }
+ **/
+router.get('/', (req, res) => { res.json({"status": "ok"}) })
+
+
+router.post('/', function(req, res) { res.send('POST handler for /api/v1 route.') })
 
 module.exports = router;
