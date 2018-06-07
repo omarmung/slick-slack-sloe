19255b191f983c7535e392a6595f7f7387df57e7
Dustin Kimmel <dkimmel@gmail.com>
2018-06-06 17:30:55 -0700

    build out class functionality

--- app.js
+++ app.js
@@ -27,15 +27,39 @@ app()
       this.gameOver = false;
       this.turn = 1;
     }
+    didThisPlayerWin(player) {
+      if ( this.winValues.some( winValue => winValue & player.squaresTotal ) ) {
+        return true
+      }
+      return false
+    }
+    didEitherPlayerWin() {
+      // check players' total occupied square values against win values list
+      return [this.player1, this.player2].some( player => this.this.didThisPlayerWin(player) )
+    }
   }
+  // 
+  // since we enumerated our squares by powers of two
+  // each is basically a binary place
+  // so our decimal total and these three-in-a-row sums show up in binary
+  // with an &, we can see if one of these is represented there
+  Game.prototype.winValues = [7, 56, 448, 73, 146, 292, 273, 84]
 
   class Player {
     constructor(symbol, handle) {
       this.handle = handle
       this.symbol = symbol
+      this.occupiedSquaresByValue = []
     }
-    checkIfExistsInChannel(channel) {
+    checkIfUserExistsInChannel(channel) {
       // check if user exists in this channel
+    }
+    get squaresTotal() {
+      // get sum of all values of all squares a player currently occupies
+      if (this.occupiedSquaresByValue.length) {
+        return this.occupiedSquaresByValue.reduce((acc, val) => acc + val ) 
+      }
+      return 0
     }
   }
 
@@ -48,8 +72,23 @@ app()
         this.squares.push(new Square(Math.pow(2, i)))
       }
     }
-    occupySquare(index){
-      if(this.squares[index]) {}
+    occupySquare(index, player){
+      let square = this.squares[index]
+      if(!square.mark) {
+        square.mark = true
+        player.occupiedSquaresByValue.push(square.val)
+        return true;
+      }
+      return false;
+    }
+    render() {
+      console.log(
+        `
+          ${this.squares[0].mark} | ${this.squares[1].mark} | ${this.squares[2].mark}
+          ${this.squares[3].mark} | ${this.squares[4].mark} | ${this.squares[5].mark}
+          ${this.squares[6].mark} | ${this.squares[7].mark} | ${this.squares[8].mark}
+        `
+      )
     }
   }
 
@@ -71,8 +110,4 @@ app()
     get val(){
       return this.value
     }
-    set val(value){
-      this.value = value 
-    }
-
   }
