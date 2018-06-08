1296673aacdccf720e932a2bc5e84537a61dd0d7
Dustin Kimmel <dkimmel@gmail.com>
2018-06-08 13:08:04 -0700

    add command parser

--- services/command_parser.js
+++ services/command_parser.js
@@ -1,14 +1,5 @@
 // parse incoming commands from slack /command POST request body text
-export function parseCommand(bodyText) {
-  
-  switch(bodyText) {
-    case n:
-      // code block
-      break;
-    case n:
-      // code block
-      break;
-    default:
-      // code block
-}
+module.exports = function parseCommand(bodyText) {
+  // parse words out of text field
+  return bodyText.split(' ')
 }
