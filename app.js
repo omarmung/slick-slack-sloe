095e862d8db79e22ec21591b3ea76ed6c00d84da
Dustin Kimmel <dkimmel@gmail.com>
2018-06-08 14:54:06 -0700

    add model/db

--- app.js
+++ app.js
@@ -11,9 +11,25 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || process.env.APP_PORT_NUMBER
 const slack_integration_token = process.env.SLACK_INTEGRATION_TOKEN
 
+// connect to 'db'
+const Workspace = require('./libs/workspace')
+const workspace = new Workspace() 
+
+// middleware check 
+const requireIntegrationToken = (req, res, next) => {
+  if(req.body.token && req.body.token === slack_integration_token) {
+    next()
+    return
+  }
+  res.sendStatus(404)
+  return
+}
 // build express app
 app()
   .use(bodyParser.json()) // attach app-wide middleware
   .use('/docs', app.static('./doc')) // attach static route for documentation
+  // .all('/api/v1/*', requireIntegrationToken)
   .use('/api/v1', v1) // attach router
   .listen(port, () => { console.log('Server listening on port ' + port + '...') } ) // start server
+
+  
