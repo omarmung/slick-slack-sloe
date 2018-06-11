c02e2941c69813d37e344bea776ef8ff01a6e0e0
Dustin Kimmel <dkimmel@gmail.com>
2018-06-11 11:18:20 -0700

    add username parser

---
+++ services/username_parser.js
@@ -0,0 +1,19 @@
+// parse username string to username obj
+module.exports = function(usernameString) {
+  // identify <@U1234|user> format
+  
+  // search
+  const regex = /\<@(.*?)\|(.*?)\>/
+  const matched = regex.exec(usernameString)
+  // handle cases
+  if (matched === null) {
+    // it's not an expanded entity
+    return false
+  }
+  // looks like the <@U1234|user> format
+  // send back a parsed obj
+  return {
+    parsedUserId: matched[1],
+    parsedUserName: matched[2]
+  }
+}
