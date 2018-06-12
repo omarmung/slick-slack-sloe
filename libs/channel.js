b575ab784a26828d6fb9e2eb12fd28aa24cd4231
Dustin Kimmel <dkimmel@gmail.com>
2018-06-12 10:48:05 -0700

    plumb names into model

--- libs/channel.js
+++ libs/channel.js
@@ -1,11 +1,11 @@
 const Game = require('../libs/game')
 
 class Channel {
-  constructor(slackChannelId, channelName, player1UserId, player1Symbol, player2Symbol) {
+  constructor(slackChannelId, channelName, player1UserId, player1Symbol, player2Symbol, player2UserId) {
     this.name = channelName
     this.slackChannelId = slackChannelId
     this.gameInProgress = true
-    this.game = new Game(player1UserId, player1Symbol, player2Symbol)
+    this.game = new Game(player1UserId, player1Symbol, player2Symbol, player2UserId)
   }
 }
 
